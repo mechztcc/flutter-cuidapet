c68c74e6e761415d87d7e30202642843feca2ab4
mechztcc <dojathan@gmail.com>
2022-02-17 17:41:19 -0300

    Added custom text field

---
+++ lib/app/core/ui/components/cuidapet_text_form_field.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CuidaPetTextFormField extends StatelessWidget {
+  final String label;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  final bool obscureText;
+  final IconButton? suffixIcon;
+  final ValueNotifier<bool> obscureTextVN;
+
+  CuidaPetTextFormField({
+    Key? key,
+    required this.label,
+    this.controller,
+    this.validator,
+    this.obscureText = false,
+    this.suffixIcon,
+  })  : obscureTextVN = ValueNotifier<bool>(obscureText),
+        assert(obscureText == true ? suffixIcon == null : true,
+            'obscureText não pode ser adicionado junto com o suffixIcon'),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          cursorColor: Colors.green,
+          controller: controller,
+          validator: validator,
+          obscureText: obscureTextValue,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: const BorderSide(color: Colors.green),
+            ),
+            suffixIcon: obscureText
+                ? IconButton(
+                    onPressed: () {
+                      obscureTextVN.value = !obscureTextValue;
+                    },
+                    icon: Icon(obscureTextValue ? Icons.lock : Icons.lock_open))
+                : suffixIcon,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/app/modules/auth/login/components/login_form.dart
+++ lib/app/modules/auth/login/components/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:app_cuidapet/app/core/ui/components/cuidapet_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatefulWidget {
@@ -12,8 +13,15 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TextFormField(),
-        TextFormField(),
+        SizedBox(height: 10),
+        CuidaPetTextFormField(
+          label: 'Login',
+        ),
+        SizedBox(height: 10),
+        CuidaPetTextFormField(
+          label: 'Senha',
+          obscureText: true,
+        ),
         const SizedBox(
           height: 20,
         ),
